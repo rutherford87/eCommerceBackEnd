27e1032c2253f1d9cfe3b8c1eed0f49071c8bf0f
Tyler Rutherford <rutherford87@gmail.com>
2021-04-29 23:39:08 -0500

    updated with get, post, put, and delete functions on categories. will revisit adding CRUD to other tables later

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,7 +37,7 @@ router.post('/', async (req, res) => {
   // create a new category
   try {
     const categoryData = await Category.create({
-      category_id: req.body.category_id,
+      category_name: req.body.category_name,
     });
     res.status(200).json(categoryData);
   } catch (err) {
