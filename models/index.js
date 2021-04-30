c4380d69b41d7ae8a68195b7e0ec4440f7460ef5
Tyler Rutherford <rutherford87@gmail.com>
2021-04-29 22:41:40 -0500

    get for all and for individual category works in insomnia. troubleshooting insomnia for update and create

--- models/index.js
+++ models/index.js
@@ -4,13 +4,33 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: 'product_id',
+  through: {
+    model: ProductTag,
+  },
+as:  'chosen_tag'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: 'tag_id',
+  through: {
+    model: ProductTag,
+  },
+as:  'chosen_product'
+})
 
 module.exports = {
   Product,
